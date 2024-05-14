40e6028548a0a6e470e8cb175ca590b582494d2a
Iván Ezequiel Iencinella <iiencinella@outlook.es>
2024-05-14 09:04:23 -0300

    Desarrollo la página

---
+++ src/data/Schedule.js
@@ -0,0 +1,68 @@
+export const Schedule = [
+  {
+    hour:'9hs',
+    description: 'Palabras de bienvenida del presidente de la Junta Arquidiocesana de Laicos, Hugo Pais',
+    resume: '',
+    link: 'https://linktr.ee/encuentrodelaicos',
+  },
+  {
+    hour:'9:15hs',
+    description: 'Apertura',
+    resume: 'A cargo del Arzobispo Mons. Juan Alberto Puiggari',
+    link: '',
+  },
+  {
+    hour:'9:30hs',
+    description: 'Momento musical',
+    resume: '',
+    link: '',
+  },
+  {
+    hour:'9:40hs',
+    description: 'Primer panel: La misión del laico en la iglesia',
+    resume: 'Lectura de trabajos testimoniales e intercambio de opiniones',
+    link: '',
+  },
+  {
+    hour:'11:30hs',
+    description: 'Segundo panel: La misión del laico en la sociedad civil',
+    resume: 'Lectura de trabajos testimoniales e intercambio de opiniones',
+    link: '',
+  },
+  {
+    hour:'12:40hs',
+    description: 'Momento musical',
+    resume: '',
+    link: '',
+  },
+  {
+    hour:'13hs',
+    description: 'Almuerzo libre',
+    resume: '',
+    link: '',
+  },
+  {
+    hour:'14hs',
+    description: 'Momento musical',
+    resume: '',
+    link: '',
+  },
+  {
+    hour:'14:15hs',
+    description: 'Tercer panel: La misión del laico en la política',
+    resume: 'Lectura de trabajos testimoniales e intercambio de opiniones',
+    link: '',
+  },
+  {
+    hour:'16:30hs',
+    description: 'Momento musical y visita a stands',
+    resume: '',
+    link: '',
+  },
+  {
+    hour:'17hs',
+    description: 'Santa misa',
+    resume: 'Presidida por nuestro Arzobispo Mons. Juan Alberto Puiggari',
+    link: '',
+  },
+]
