1c5a2324054c501f7eaccf16690823686f4d1c81
Iván Ezequiel Iencinella <iiencinella@outlook.es>
2024-05-17 11:12:27 -0300

    Añado sección de escritos

---
+++ src/data/writers.js
@@ -0,0 +1,20 @@
+export const Writers = [
+  {
+    title: 'Los laicos en la iglesia',
+    image: '/church.svg',
+    resume: 'resumen 1',
+    link: 'https://linktr.ee/encuentrodelaicos',
+  },
+  {
+    title: 'Los laicos en la sociedad',
+    image: '/society.svg',
+    resume: 'resumen 2',
+    link: 'https://linktr.ee/encuentrodelaicos',
+  },
+  {
+    title: 'Los laicos en la politica',
+    image: '/politic.svg',
+    resume: 'resumen 3',
+    link: 'https://linktr.ee/encuentrodelaicos',
+  },
+]
