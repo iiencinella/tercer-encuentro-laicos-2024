4eb54b79f9139e1e44950851bc0e7f6dbd0f2c75
Iván Ezequiel Iencinella <iiencinella@outlook.es>
2024-05-18 02:53:05 -0300

    Añado escritos de los expositores

--- src/data/writers.js
+++ src/data/writers.js
@@ -2,19 +2,89 @@ export const Writers = [
   {
     title: 'Los laicos en la iglesia',
     image: '/church.svg',
-    resume: 'resumen 1',
-    link: 'https://linktr.ee/encuentrodelaicos',
-  },
-  {
-    title: 'Los laicos en la sociedad',
-    image: '/society.svg',
-    resume: 'resumen 2',
-    link: 'https://linktr.ee/encuentrodelaicos',
+    writable: [
+      {
+        title: 'Legión de María: "Un trabajo activo y sólido en unión con nuestra Reina y Madre"',
+        resume: 'Historia y actividades de la Legión de María, en su trabajo cotidiano como servicio para la iglesia y la sociedad.',
+        link: 'https://drive.google.com/file/d/1seAVIUxC9nNRqoAtD_awD9SlU9JLOEAV/view?usp=drive_link',
+      },
+      {
+        title: 'Los laicos en la Iglesia "Id también vosotros" - Hugo Cabrera, Cáritas',
+        resume: 'Llamado del fiel laico dentro de la iglesia y su rol en la evangelización y la misión.',
+        link: 'https://drive.google.com/file/d/14fVERHDW7Dql6k6PfB7tRE0nFFg_gia4/view?usp=sharing',
+      },
+      {
+        title: 'OVE Santa Teresita del Niño Jesús de Paraná',
+        resume: 'Misión y carisma de la Obra por las Vocaciones Eclesiásticas(OVE)',
+        link: 'https://drive.google.com/file/d/1TWyGy4svqNypmt-SVAtHUVUSV4s7A7cd/view?usp=sharing',
+      },
+      {
+        title: 'El lugar que ocupamos en el plan de Dios - Rocio Padilla',
+        resume: 'Las personas en condición de discapacidad, su lugar dentro de la Iglesia y su participación en la vida de la Fe.',
+        link: 'https://drive.google.com/file/d/1UxvMGprw9bFgNLmM9cpWElbFHPf7eliB/view?usp=sharing',
+      },
+      {
+        title: 'Todos, Todos, Todos - Rocio Padilla',
+        resume: 'La misión de los jóvenes en la iglesia, la falta de vocaciones y desafíos ante esta realidad. Una mirada desde la Pastoral de Juventud.',
+        link: 'https://drive.google.com/file/d/190S7b4x2tsgHZbV6cX9rHv3sdhGUtTep/view?usp=sharing',
+      },
+      {
+        title: 'Un Dios vivo y operante en lo cotidiano - Celia Lafourcade',
+        resume: 'Experiencia personal de la vida de oración y cercanía a Dios en el día a día a través de Convivencia con Dios.',
+        link: 'https://drive.google.com/file/d/1N6yW5ku2V6AC9dSaO0Lqi5DadJfd1IoZ/view?usp=sharing',
+      },
+      {
+        title: 'Qué misión tan bella es ser apóstol - Maria Eugenia Sambrana',
+        resume: 'Ministros Extraordinarios de la Comunión(MEC), una reflexión personal.',
+        link: 'https://drive.google.com/file/d/1Ok-ouun4cClrC5vA2sOD6Kj14JjsjieC/view?usp=sharing',
+      },
+    ],
   },
   {
     title: 'Los laicos en la politica',
     image: '/politic.svg',
-    resume: 'resumen 3',
-    link: 'https://linktr.ee/encuentrodelaicos',
+    writable: [
+      {
+        title: 'El laico en la vida pública y en la política.Aquí, ahora. - Samuel Madrid Paez',
+        resume: 'Desafíos y rol del laico en la participación política, como misión y compromiso para con la sociedad.',
+        link: 'https://drive.google.com/file/d/1UuDqTJMIkQbc7KB7F-DldjTEbEJKfz4u/view?usp=sharing',
+      },
+      {
+        title: 'Necesidad de conocer y aplicar la doctrina social a la política - Juan Carlos Reffino Castagno',
+        resume: 'Doctrina social de la iglesia y su aplicación dentro de la política.',
+        link: 'https://drive.google.com/file/d/1KlV01eCfe5SSjj9MMgbLwPitRlpU-UOn/view?usp=sharing',
+      },
+      {
+        title: 'La sociedad civil puede ejercer un mayor control en sus gobernantes - Hugo Román Barrios',
+        resume: 'La sociedad civil y su función dentro de la realidad del sistema político actual, posibles soluciones y mejoras.',
+        link: 'https://drive.google.com/file/d/1TvD_eRJFbltdPCLc7LXF2t75-lJZEj76/view?usp=sharing',
+      },
+    ],
+  },
+  {
+    title: 'Los laicos en la sociedad',
+    image: '/society.svg',
+    writable: [
+      {
+        title: 'Ser laico en el Poder Judicial - Maria Eugenia Sambrana',
+        resume: 'Reflexión personal acerca del trabajo en el poder judicial y los desafíos de esta realidad.',
+        link: 'https://drive.google.com/file/d/1cSo3RpwNNuApR0ykwj7UV8e2oNklbp5R/view?usp=sharing',
+      },
+      {
+        title: 'Salir de las adicciones encontrando en mi vida al mejor “médico”: Cristo - Pastoral de Adicciones',
+        resume: 'Experiencia de vida del trabajo y acción de Casa Lázaro y la función del laico en la recuperación de adicciones.',
+        link: 'https://drive.google.com/file/d/1XT6nl6-RBqoBn7U0VUz0lVh7InrB1o6Q/view?usp=sharing',
+      },
+      {
+        title: 'Casita Estrella - Movimiento Focolares',
+        resume: 'ctividad y función dentro de la comunidad del barrio “El brete” y la zona costera del apostolado “Casita Estrella”',
+        link: 'https://drive.google.com/file/d/1XmpzUC6O6LvMX3ArjpGGwtqX-KTneNX/view?usp=sharing',
+      },
+      {
+        title: 'Educación en el hogar, una opción para estos tiempos - María Elisa Pacher y Augusto N.Bompadre',
+        resume: 'La educación en casa como opción de vida para la educación dentro de la familia',
+        link: 'https://drive.google.com/file/d/1UeMuYpCZmGRXSAX3Aplv-IBi7ovhEdt3/view?usp=sharing',
+      },
+    ],
   },
 ]
